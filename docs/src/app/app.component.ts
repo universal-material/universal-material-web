0942f7984799d321e3d9eff0ad5dde67a52e0918
Hideyuki <erichideyuki@gmail.com>
2024-04-16 13:21:07 -0300

    Added color selection to themes

--- docs/src/app/app.component.ts
+++ docs/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 import { LinkActiveDirective } from './docs/link-active.directive';
 import { SubmenuComponent } from './docs/submenu/submenu.component';
+import { ThemeBuilder } from '@universal-material/web';
+import { interval, Subject, throttle, throttleTime } from 'rxjs';
 
 enum ThemeMode {
   Auto,
@@ -13,6 +17,7 @@ enum ThemeMode {
   selector: 'docs-root',
   standalone: true,
   imports: [
+    FormsModule,
     RouterLink,
     RouterLinkActive,
     LinkActiveDirective,
@@ -27,14 +32,45 @@ export class AppComponent {
   ThemeMode = ThemeMode;
 
   currentThemeMode = parseInt(localStorage['currentThemeMode'], 10) || 0;
-  toggleMenu = false;
+  themeColor = '#6750a4';
+  $themeColorChange = new Subject<string>();
 
   constructor() {
     this.applyThemeMode();
+
+    const storedThemeColor = localStorage['currentThemeColor'];
+
+    if (storedThemeColor) {
+      this.setThemeColor(storedThemeColor);
+    }
+
+    this.$themeColorChange
+      .pipe(throttleTime(1000, undefined, {leading: true, trailing: true}))
+      .subscribe(color => this.setThemeColor(color));
+
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', () => this.applyThemeMode());
   }
 
   toggleRtl() {
     document.body.classList.toggle("rtl");
+  }
+
+  setThemeColor(color: string) {
+    const styles = ThemeBuilder.create(color).build();
+
+    let themeStylesElement = document.getElementById('theme-styles');
+
+    if (!themeStylesElement) {
+      themeStylesElement = document.createElement('style');
+      themeStylesElement.id = 'theme-styles';
+      document.head.appendChild(themeStylesElement);
+    }
+
+    themeStylesElement.innerText = styles;
+    localStorage['currentThemeColor'] = color;
+    this.themeColor = color;
   }
 
   setThemeMode(mode: ThemeMode) {
@@ -44,22 +80,14 @@ export class AppComponent {
   }
 
   private applyThemeMode() {
-    const darkMode = this.currentThemeMode === ThemeMode.Dark;
-    const lightMode = this.currentThemeMode === ThemeMode.Light;
-
-    if (!darkMode && !lightMode) {
-      document.documentElement.classList.remove('u-dark-mode');
-      document.documentElement.classList.remove('u-light-mode');
-      return;
-    }
+    const darkMode = this.currentThemeMode === ThemeMode.Dark ||
+      this.currentThemeMode === ThemeMode.Auto && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
     if (!darkMode) {
-      document.documentElement.classList.add('u-light-mode');
       document.documentElement.classList.remove('u-dark-mode');
       return;
     }
 
     document.documentElement.classList.add('u-dark-mode');
-    document.documentElement.classList.remove('u-light-mode');
   }
 }
